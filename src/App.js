4ce284eb0855fb1956ec7951094ca572540de018
Dementiy <maxim.silov.2016@gmail.com>
2021-05-20 19:17:18 +0300

    Created SignIn and SignUp components
    - Added react-router-dom in App component

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import SignUp from './Models/Components/SignUp';
+import SignIn from './Models/Components/SignIn';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/SignIn">Sign in</Link>
+            </li>
+            <li>
+              <Link to="/SignUp">Sign up</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/SignIn">
+            <SignIn />
+          </Route>
+          <Route path="/SignUp">
+            <SignUp />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Common/MyTextField.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useField } from 'formik';
+import { TextField } from '@material-ui/core';
+
+const MyTextField = (props) => {
+    const [field, meta] = useField(props);
+    return (
+        <TextField {...field} {...props} error={meta.touched && meta.error != null} helperText={meta.touched && meta.error ? meta.error : null}/>
+    );
+}
+
+export default MyTextField;
